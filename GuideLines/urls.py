379d784666b5c2b1efa2591b72f06b9c68063b19
GavrikGal <gavrik.gal@gmail.com>
2022-10-05 16:12:34 +0300

    GL-22 Сохраняются ли аватарки? Да

--- GuideLines/urls.py
+++ GuideLines/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.decorators.cache import never_cache
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('django.contrib.auth.urls')),
     path('', include('guides.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>', never_cache(serve)))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- guides/models.py
+++ guides/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    avatar = models.ImageField(upload_to='',
+    avatar = models.ImageField(upload_to='avatars',
                                null=True,
                                blank=True)
 
