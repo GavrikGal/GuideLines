b1fa2aea591d23bca8cf07c72eb6568f7a37ca46
GavrikGal <gavrik.gal@gmail.com>
2022-10-05 09:17:41 +0300

    GL-5 Страница регистрации. FT. Тест на регистрацию пользователя. Добавлена модель CustomUser

--- GuideLines/settings.py
+++ GuideLines/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'guides.CustomUser'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
--- guides/models.py
+++ guides/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(upload_to='',
+                               null=True,
+                               blank=True)
+
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
