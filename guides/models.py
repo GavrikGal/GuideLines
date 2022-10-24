ab6d3062261e2cf483ea0092dd884a8584fac5ec
GavrikGal <gavrik.gal@gmail.com>
2022-10-24 09:55:48 +0300

    GL-9 Простое Руководство. Рефакторизация. Добавление UT для CustomUserModel. Тест загрузки аватарки

--- guides/models.py
+++ guides/models.py
@@ -15,11 +15,24 @@ class CustomUser(AbstractUser):
                                blank=True)
 
     def get_upload_path(self, filename):
-        return "avatar/" + str(self.username) + "/" \
+        return str(self.username) + "/avatar/" \
                + splitext(filename)[0] + '.' + str(datetime.now().timestamp()) + splitext(filename)[1]
 
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
-    # + str(datetime.now().timestamp())
 
+class Guide(models.Model):
+    """Модель Руководства"""
 
+    name = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(blank=True)
+    cover = models.ImageField(upload_to='media',
+                              null=True,
+                              blank=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+
+    def get_upload_path(self, filename):
+        return str(self.author.username) + "/guides/" + str(self.name) + '/' \
+               + splitext(filename)[0] + '.' + str(datetime.now().timestamp()) + splitext(filename)[1]
+
+    REQUIRED_FIELDS = ['name']
--- guides/tests.py
+++ guides/tests.py
@@ -1,3 +1,46 @@
+from os.path import splitext
+from shutil import rmtree
+
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 
-# Create your tests here.
+from .models import CustomUser
+
+
+TEST_USERNAME = 'gal_test'
+TEST_PASSWORD = 'Password12345'
+TEST_FIRST_NAME = 'Дмитрий'
+TEST_LAST_NAME = 'Гал'
+TEST_AVATAR_IMG_NAME = 'gal-avatar-12345.jpg'
+
+TEST_MEDIA_CONTENT = '/functional_tests/img/Avatar.jpg'
+
+
+class UnitTest(TestCase):
+    """Модульное тестирование"""
+
+    def test_can_start_test(self) -> None:
+        """тесты могут запускаться"""
+        self.assertEqual(1 + 2, 3)
+
+
+class CustomUserModelTest(TestCase):
+    """Модульное тестирование модули пользователя"""
+    def test_custom_user_avatar_upload_path(self) -> None:
+        """тест пути загрузки аватарки пользователя"""
+        user = CustomUser.objects.create(username=TEST_USERNAME,
+                                         first_name=TEST_FIRST_NAME,
+                                         last_name=TEST_LAST_NAME,
+                                         avatar=TEST_AVATAR_IMG_NAME)
+
+        user.avatar = SimpleUploadedFile(TEST_AVATAR_IMG_NAME, b'', content_type='image/jpeg')
+        user.save()
+        self.assertIn(
+            user.username + '/avatar/' + splitext(TEST_AVATAR_IMG_NAME)[0],
+            user.avatar.name
+        )
+
+    def tearDown(self):
+        rmtree(settings.MEDIA_ROOT / TEST_USERNAME, ignore_errors=True)
+
