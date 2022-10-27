b43c2c7ad3eb1730a3972eed4561ca95cbd58e54
GavrikGal <gavrik.gal@gmail.com>
2022-10-27 16:35:56 +0300

    GL-26 Рефакторинг. При удалении поста, следует удалить соответствующие медиаданные. В процессе

--- guides/models.py
+++ guides/models.py
@@ -3,6 +3,8 @@ from os.path import splitext
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 def _upload_path(instance, filename):
@@ -35,14 +37,26 @@ class Guide(models.Model):
                                verbose_name='Автор')
 
     def get_upload_path(self, filename):
-        return str(self.author.username) + "/guides/" + str(self.name) + '/' \
-               + splitext(filename)[0] + '.' + str(datetime.now().timestamp()) + splitext(filename)[1]
+        return str(self.author.username) + "/guides/" + str(self.name) + '__' \
+               + splitext(filename)[0] + '__' + str(datetime.now().timestamp()) + splitext(filename)[1]
 
     REQUIRED_FIELDS = ['name']
 
     def __str__(self):
-        return 'Гайд'
+        return self.name
 
     class Meta:
         verbose_name = 'Гайд'
         verbose_name_plural = 'Гайды'
+
+
+@receiver(pre_delete, sender=Guide)
+def cover_delete(sender, instance, **kwargs):
+    if instance.cover.name:
+        instance.cover.delete(False)
+
+
+@receiver(pre_delete, sender=CustomUser)
+def avatar_delete(sender, instance, **kwargs):
+    if instance.avatar.name:
+        instance.avatar.delete(False)
