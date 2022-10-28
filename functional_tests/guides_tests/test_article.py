cc2314d64ec0d0c3362a1f1ccef5d587893ba7ac
GavrikGal <gavrik.gal@gmail.com>
2022-10-28 14:34:26 +0300

    GL-11 Простая Статья. FT. Начало теста

---
+++ functional_tests/guides_tests/test_article.py
@@ -0,0 +1,10 @@
+from functional_tests.base import FunctionalTest
+
+
+class ArticleTest(FunctionalTest):
+    """тесты Статей"""
+
+    def test_can_create_new_article(self) -> None:
+        """тест можно создать новую статью"""
+
+        self.fail("Доделать")
