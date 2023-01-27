4481272047f58ec3b01c9e7eff77b109876dc170
GavrikGal <gavrik.gal@gmail.com>
2023-01-27 08:21:12 +0300

    GL-80 Aвтозапуск FT. Настройка GitHub-Actions. Failed test 10

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -79,8 +79,8 @@ class FunctionalTest(LiveServerSingleThreadedTestCase):
             options.add_argument('--disable-gpu')
         self.browser = webdriver.Firefox(options=options)
         # self.browser.set_window_size(5120, 2880)
-        # self.browser.set_window_size(1920, 1080)
-        self.browser.set_window_size(320, 480)
+        self.browser.set_window_size(1920, 1080)
+        # self.browser.set_window_size(320, 480)
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
